f60b868096223b87e6ec7698dda4a2eb157b3fba
Alexande B <abobrikovich@gmail.com>
2023-04-05 18:13:59 +0200

    Move to direct fetch code instead systen

--- crawler.rb
+++ crawler.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 require 'fileutils'
-
-t1 = Time.now
-puts "Started at: #{t1}"
+require 'relaton_ietf'
 
 FileUtils.rm_rf("data")
-system("relaton fetch-data ietf-rfc-entries")
-
-t2 = Time.now
-puts "Stopped at: #{t2}"
-puts "Done in: #{(t2 - t1).round} sec."
+RelatonIetf::DataFetcher.fetch("ietf-rfc-entries")
