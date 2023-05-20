242f9a615ec796cdabc1ed234482b7de4f0bd5bf
Andrei Kislichenko <android.2net@gmail.com>
2023-05-19 23:06:21 -0400

    create index

--- crawler.rb
+++ crawler.rb
@@ -4,4 +4,9 @@ require 'fileutils'
 require 'relaton_ietf'
 
 FileUtils.rm_rf("data")
+FileUtils.rm Dir.glob("index*")
+
 RelatonIetf::DataFetcher.fetch("ietf-rfc-entries")
+
+system "zip index-v1.zip index-v1.yaml"
+system "git add index-v1.zip index-v1.yaml"
