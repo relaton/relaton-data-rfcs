ec3e697feeaa9dccb548ac827ba3cb5ef01af35f
Andrei Kislichenko <android.2net@gmail.com>
2023-06-07 19:41:47 -0400

    Update crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -7,6 +7,3 @@ FileUtils.rm_rf("data")
 FileUtils.rm Dir.glob("index*")
 
 RelatonIetf::DataFetcher.fetch("ietf-rfc-entries")
-
-system "zip index-v1.zip index-v1.yaml"
-system "git add index-v1.zip index-v1.yaml"
